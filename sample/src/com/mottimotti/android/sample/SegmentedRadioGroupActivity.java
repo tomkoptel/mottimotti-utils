f3d2f9359fb8d15cb2e982695d2a7d59b3926d5a
Tom Koptel <tom.koptel@gmail.com>
2013-04-19 23:00:56 +0300

    Extending segmented radio group sample

--- sample/src/com/mottimotti/android/sample/SegmentedRadioGroupActivity.java
+++ sample/src/com/mottimotti/android/sample/SegmentedRadioGroupActivity.java
@@ -1,32 +1,13 @@
 package com.mottimotti.android.sample;
 
 import android.os.Bundle;
-import android.os.Parcelable;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.mottimotti.android.R;
-import com.mottimotti.android.widget.RadioGroupSegmented;
 
 public class SegmentedRadioGroupActivity extends SherlockActivity {
-    private RadioGroupSegmented radioGroupSegmented;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sample_segmented_group);
-        radioGroupSegmented = (RadioGroupSegmented) findViewById(R.id.radio_group_segmented);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        Parcelable parcelable = radioGroupSegmented.onSaveInstanceState();
-        outState.putParcelable("radio_group", parcelable);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onRestoreInstanceState(Bundle savedState) {
-        super.onRestoreInstanceState(savedState);
-        Parcelable parcelable = savedState.getParcelable("radio_group");
-        radioGroupSegmented.onRestoreInstanceState(parcelable);
     }
 }
