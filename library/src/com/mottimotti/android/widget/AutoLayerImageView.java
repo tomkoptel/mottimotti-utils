15825246d57a983458f7803ef9ffc4442da721d7
Tom Koptel <tom.koptel@gmail.com>
2013-08-08 13:21:37 +0300

    Fixing AutoLayerImageView

--- library/src/com/mottimotti/android/widget/AutoLayerImageView.java
+++ library/src/com/mottimotti/android/widget/AutoLayerImageView.java
@@ -12,25 +12,23 @@ import com.mottimotti.android.R;
 public class AutoLayerImageView extends ImageView {
     private PorterDuff.Mode[] modes = new PorterDuff.Mode[]{
             PorterDuff.Mode.XOR, // 0
-            PorterDuff.Mode.ADD, // 1
-            PorterDuff.Mode.CLEAR, // 2
-            PorterDuff.Mode.DARKEN, // 3
-            PorterDuff.Mode.DST,    // 4
-            PorterDuff.Mode.DST_ATOP, // 5
-            PorterDuff.Mode.DST_IN,   // 6
-            PorterDuff.Mode.DST_OUT,  // 7
-            PorterDuff.Mode.DST_OVER, // 8
-            PorterDuff.Mode.LIGHTEN,  // 9
-            PorterDuff.Mode.MULTIPLY, // 10
-            PorterDuff.Mode.OVERLAY,  // 11
-            PorterDuff.Mode.SCREEN,   // 12
-            PorterDuff.Mode.SRC,      // 13
-            PorterDuff.Mode.SRC_ATOP, // 14
-            PorterDuff.Mode.SRC_IN,   // 15
-            PorterDuff.Mode.SRC_OUT,  // 16
-            PorterDuff.Mode.SRC_OVER  // 17
+            PorterDuff.Mode.CLEAR, // 1
+            PorterDuff.Mode.DARKEN, // 2
+            PorterDuff.Mode.DST,    // 3
+            PorterDuff.Mode.DST_ATOP, // 4
+            PorterDuff.Mode.DST_IN,   // 5
+            PorterDuff.Mode.DST_OUT,  // 6
+            PorterDuff.Mode.DST_OVER, // 7
+            PorterDuff.Mode.LIGHTEN,  // 8
+            PorterDuff.Mode.MULTIPLY, // 9
+            PorterDuff.Mode.SCREEN,   // 10
+            PorterDuff.Mode.SRC,      // 11
+            PorterDuff.Mode.SRC_ATOP, // 12
+            PorterDuff.Mode.SRC_IN,   // 13
+            PorterDuff.Mode.SRC_OUT,  // 14
+            PorterDuff.Mode.SRC_OVER  // 15
     };
-    private int mode = 12;
+    private int mode = 10;
     private int colorMode = android.R.color.transparent;
 
     public AutoLayerImageView(Context context) {
@@ -51,7 +49,7 @@ public class AutoLayerImageView extends ImageView {
 
     private void init(Context context, AttributeSet attrs) {
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AutoLayerImageView);
-        mode = a.getInt(R.styleable.AutoLayerImageView_duff, 12);
+        mode = a.getInt(R.styleable.AutoLayerImageView_duff, 10);
         colorMode = a.getInt(R.styleable.AutoLayerImageView_color_mode, getResources().getColor(android.R.color.transparent));
     }
 
